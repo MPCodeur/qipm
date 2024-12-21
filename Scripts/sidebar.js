554e2c6f908d2e4ba6eeed6ee8b77c01f72728a6
MPCodeur <pugliese.2c@gmail.com>
2024-12-21 17:00:23 +0100

    overlay rajouté

--- Scripts/sidebar.js
+++ Scripts/sidebar.js
@@ -1,8 +1,10 @@
 function toggleSidebar() {
     const sidebar = document.getElementById('sidebar');
     if (sidebar.style.right === '0px') {
-        sidebar.style.right = '-200px';
+        sidebar.style.right = '-300px';
+        document.getElementById('overlay').style.display = 'none';
     } else {
-        sidebar.style.right = '0px'; 
+        sidebar.style.right = '0px';
+        document.getElementById('overlay').style.display = 'block';
     }
 }
