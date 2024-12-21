e3cfcd840cbdec858d2be9bf659e8e40334ae1aa
MPCodeur <pugliese.2c@gmail.com>
2024-12-21 16:26:11 +0100

    sidebar v1

--- Scripts/sidebar.js
+++ Scripts/sidebar.js
@@ -1,7 +1,7 @@
 function toggleSidebar() {
     const sidebar = document.getElementById('sidebar');
     if (sidebar.style.right === '0px') {
-        sidebar.style.right = '-300px';
+        sidebar.style.right = '-200px';
     } else {
         sidebar.style.right = '0px'; 
     }
