26fc097e2b95fbf1609839d52a982c1eb2c4a542
MPCodeur <pugliese.2c@gmail.com>
2024-12-22 15:55:16 +0100

    sidebar media

--- Scripts/sidebar.js
+++ Scripts/sidebar.js
@@ -1,10 +1,15 @@
 function toggleSidebar() {
     const sidebar = document.getElementById('sidebar');
-    if (sidebar.style.right === '0px') {
-        sidebar.style.right = '-300px';
+
+    if (sidebar.style.right === '0px' && window.innerWidth > 768) {
+        sidebar.style.right = '-20%';
+        document.getElementById('overlay').style.display = 'none';
+    } else if (sidebar.style.right === '0px' && window.innerWidth < 768) {
+        sidebar.style.right = '-60%';
         document.getElementById('overlay').style.display = 'none';
     } else {
         sidebar.style.right = '0px';
         document.getElementById('overlay').style.display = 'block';
     }
+
 }
